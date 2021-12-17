bcc50283775c93c793e9b58a38982c52ee7749d7
DERE KEVIN <kevin.dere@univ-reims.fr>
2021-12-17 08:19:16 +0100

    commit before new feature

--- src/ConnectionLDAP.php
+++ src/ConnectionLDAP.php
@@ -3,6 +3,7 @@
 namespace hakuryo\ldap;
 
 use Exception;
+use hakuryo\ldap\LdapBatchModification as LdapLdapBatchModification;
 use hakuryo\ldap\traits\ActiveDirectoryOperation;
 use hakuryo\ldap\utils\ConfigParser;
 use JsonException;
@@ -12,16 +13,18 @@ use JsonException;
  *
  * @author Hakuryo
  */
-class ConnectionLDAP {
+class ConnectionLDAP
+{
 
     use ActiveDirectoryOperation;
     use LdapUtils;
-    
+
     const MOD_ADD = 0;
     const MOD_REPLACE = 1;
     const MOD_DEL = 2;
 
-        /**
+
+    /**
      * AD user_account_control value
      */
     const USER_ACCOUNT_ENABLE = 0x0001;
@@ -72,7 +75,8 @@ class ConnectionLDAP {
      * @param \hakuryo\ldap\LdapSearchOptions $search_options an instance of LdapSearchOptions
      * @throws Exception if LDAP server can't be conntacted or if the connection using user's credentials fail.
      */
-    public function __construct(string $host, string $login, string $password, LdapSearchOptions $search_options = null) {
+    public function __construct(string $host, string $login, string $password, LdapSearchOptions $search_options = null)
+    {
         if ($this->connection = ldap_connect($host)) {
             ldap_set_option($this->connection, LDAP_OPT_PROTOCOL_VERSION, 3);
             if (!@ldap_bind($this->connection, $login, $password)) {
@@ -93,7 +97,8 @@ class ConnectionLDAP {
      * @throws Exception
      * @return \hakuryo\ldap\ConnectionLDAP
      */
-    public static function from_file(string $path, string $section = null): ConnectionLDAP {
+    public static function from_file(string $path, string $section = null): ConnectionLDAP
+    {
         $conf = ConfigParser::parse_config_file($path, $section);
         $ldap = new ConnectionLDAP($conf->host, $conf->user, $conf->pwd, new LdapSearchOptions($conf->base_dn));
         return $ldap;
@@ -106,7 +111,8 @@ class ConnectionLDAP {
      * @see ldap_search
      * @return array Tableau associatif avec les noms d'attributs ldap en tant que clef.
      */
-    public function search(string $filter, array $returnedAttrs = ['*']): array {
+    public function search(string $filter, array $returnedAttrs = ['*']): array
+    {
         $research = false;
         if ($this->search_options->get_scope() === LdapSearchOptions::SEARCH_SCOPE_SUB) {
             $research = @ldap_search($this->connection, $this->search_options->get_base_dn(), utf8_encode($filter), $returnedAttrs, 0, $this->search_options->get_result_limit());
@@ -126,23 +132,25 @@ class ConnectionLDAP {
      * @param array $returnedAttrs [Optionnel] Tableau d'attribut a retourner. Defaut = ['*']
      * @return stdclass retourne un stdClass vide si aucun resultat ne correspond a $filter
      */
-    public function get_entry(string $filter, array $returnedAttrs = ['*']): \stdclass {
+    public function get_entry(string $filter, array $returnedAttrs = ['*']): \stdclass
+    {
         $limit = $this->search_options->get_result_limit();
         $this->search_options->set_result_limit(1);
         $res = $this->search($filter, $returnedAttrs);
         $this->search_options->set_result_limit($limit);
         return count($res) > 0 ? $res[0] : new \stdClass();
     }
-    
+
     /**
      * Modify a LDAP entry
      * @param string $entry_dn The distinguished name of the entry
-     * @param array $target_entry_attr The attributes to modify and there values
+     * @param array | LdapBatchModification $target_entry_attr The attributes to modify and there values or a LdapBatchModification object
      * @param int $modify_type can be one of the constant MOD_ADD,MOD_DEL,MOD_REPLACE of ConnectionLDAP
      * @return bool Return True on success.
      * @throws Exception throw an exception on fail.
      */
-    public function modify(string $entry_dn, array $target_entry_attr, int $modify_type): bool {
+    public function modify(string $entry_dn, $target_entry_attr, int $modify_type): bool
+    {
         $result = false;
         switch ($modify_type) {
             case self::MOD_ADD:
@@ -160,7 +168,7 @@ class ConnectionLDAP {
         }
         return $result;
     }
-    
+
     /**
      * Create an LDAP entry
      * @param string $entry_dn The distinguished name of the entry
@@ -168,7 +176,8 @@ class ConnectionLDAP {
      * @return bool Return True on success.
      * @throws Exception throw an exception on fail.
      */
-    public function add(string $entry_dn, array $ldap_entry_attr): bool {
+    public function add(string $entry_dn, array $ldap_entry_attr): bool
+    {
         if (!@ldap_add($this->connection, $entry_dn, $ldap_entry_attr)) {
             throw new Exception("Can't add ldap entry $entry_dn cause : " . $this->getLastError());
         }
@@ -181,7 +190,8 @@ class ConnectionLDAP {
      * @return bool Return True on success.
      * @throws Exception throw an exception on fail.
      */
-    public function delete(string $entry_dn): bool {
+    public function delete(string $entry_dn): bool
+    {
         if (!@ldap_delete($this->connection, $entry_dn)) {
             throw new Exception("Can't delete ldap entry $entry_dn cause : " . $this->getLastError());
         }
@@ -191,13 +201,14 @@ class ConnectionLDAP {
     /**
      * Close the ldap connection
      */
-    public function disconect() {
+    public function disconect()
+    {
         ldap_close($this->connection);
     }
 
     // Getter & Setter
-    public function get_search_options(): LdapSearchOptions {
+    public function get_search_options(): LdapSearchOptions
+    {
         return $this->search_options;
     }
-
 }
